52bbf78c4e5d1572a891c58c2134dedf6de9d79c
Oliver Jaffe <oliver.jaffe@hotmail.co.uk>
2022-11-27 14:45:57 +0000

    Improved data downloading

--- data/download/download_mscz.ps1
+++ data/download/download_mscz.ps1
@@ -6,7 +6,14 @@ If(!(test-path -PathType container $path))
 
 Import-Csv ".\csv_file.csv" |
     ForEach-Object -Parallel {
-        Invoke-WebRequest "https://infura-ipfs.io$($_.ref)" -O ".\files\$($_.id).mscz"
+        if (!(Test-Path ".\files\$($_.id).mscz")) {
+            try {
+                Invoke-WebRequest "https://infura-ipfs.io$($_.ref)" -O ".\files\$($_.id).mscz"
+                echo "Downloaded .\files\$($_.id).mscz"
+            } catch {
+                echo "Error in downloading"
+            }
+        }
     } -ThrottleLimit 16
 
 
